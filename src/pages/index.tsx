227761c3af36e49afab8ff46c7bf060930ecf1e1
Zong Wei Hong <p458732@kimo.com>
2020-11-27 21:31:48 +0800

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,6 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./scheduler/App"
+export default function Home() {
+  return <App />
+}
---
+++ src/pages/utils/interface.tsx
@@ -0,0 +1,12 @@
+export interface ICar {
+  name: string
+  move(): void
+}
+export interface IYCar {
+  name: string
+  move(): void
+}
+export interface IECar {
+  name: string
+  move(): void
+}
