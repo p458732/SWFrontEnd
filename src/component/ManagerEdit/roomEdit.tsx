e9076c15ab90a4fd1ca1a2b532c4d54abfd90dca
plusgood-steven <plusgoodwu@gmail.com>
2020-11-30 00:28:44 +0800

    update EditRoom

--- src/component/ManagerEdit/roomEdit.tsx
+++ src/component/ManagerEdit/roomEdit.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-console */
 /* eslint-disable @typescript-eslint/no-implied-eval */
 /* eslint-disable react/jsx-no-undef */
 /* eslint-disable react/jsx-props-no-spreading */
@@ -5,7 +7,7 @@
 /* eslint-disable @typescript-eslint/no-useless-constructor */
 /* eslint-disable react/prefer-stateless-function */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import "antd/dist/antd.css"
 import "./Edit.css"
 import { FormInstance } from "antd/lib/form"
@@ -26,12 +28,12 @@ import {
   Col,
   Modal,
 } from "antd"
-import { Room } from "../utils/interface"
+import { Room, User } from "../utils/interface"
 
 const { Option } = Select
 
 const tailLayout = {
-  wrapperCol: { offset: 14, span: 16 },
+  wrapperCol: { offset: 16, span: 16 },
 }
 const layout = {
   labelCol: { span: 4, offset: 2 },
@@ -40,61 +42,65 @@ const layout = {
 
 const { confirm } = Modal
 
-interface Mystate {
-  selectedRoom: Room
-  roomList: Array<Room>
-}
-
 interface Props {
   type: string
+  roomList: Array<Room>
+  setRoomList: React.Dispatch<React.SetStateAction<Room[]>>
+  setvisible: React.Dispatch<React.SetStateAction<boolean>>
+  visible?: boolean
 }
 
-class RoomEdit extends React.Component<Props, Mystate> {
-  formRef = React.createRef<FormInstance>()
+const fakedata = [
+  {
+    name: "TR200",
+    capacity: 12,
+  },
+  {
+    name: "TR500",
+    capacity: 10,
+  },
+  {
+    name: "RB500",
+    capacity: 20,
+  },
+]
 
-  constructor(props: any) {
-    super(props)
-    this.state = {
-      selectedRoom: {
-        name: "",
-        capacity: 0,
-      },
-      roomList: [
-        {
-          name: "TR200",
-          capacity: 12,
-        },
-        {
-          name: "TR500",
-          capacity: 10,
-        },
-        {
-          name: "RB500",
-          capacity: 20,
-        },
-      ],
-    }
-  }
+function RoomEdit(props: Props) {
+  const changeData: Room = { name: "", capacity: 0 }
+  const { type, visible, roomList, setRoomList } = props
+  const [form] = Form.useForm()
+  const [selectedRoom, setSelectRoom] = useState<Room>({
+    name: "",
+    capacity: 0,
+  })
+  useEffect(
+    function updateFrom() {
+      form.setFieldsValue({ roomName: selectedRoom.name, Capacity: selectedRoom.capacity })
+      changeData.name = selectedRoom.name
+      changeData.capacity = selectedRoom.capacity
+      console.log(selectedRoom)
+    },
+    [selectedRoom]
+  )
 
-  onFinish = (values: any) => {
-    console.log(values)
-  }
+  // const [roomList, setRoomList] = useState<Array<Room>>(fakedata)
+  useEffect(
+    function showChangedRoomlist() {
+      form.setFieldsValue({ select: selectedRoom.name })
+    },
+    [roomList]
+  )
+  const isVisible = props.visible
+  const setIsVisible = props.setvisible
 
-  onSave = () => {
-    this.showPromiseConfirm()
-  }
-
-  onCancel = () => {}
-
-  onSelectRoom = (value: string) => {
-    this.state.roomList.forEach(item => {
-      if (item.name === value) {
-        this.setState({ selectedRoom: item })
-      }
+  function showErrorMessage(message: string) {
+    Modal.error({
+      title: "Error",
+      content: message,
     })
   }
 
-  showPromiseConfirm = () => {
+  function showPromiseConfirm() {
     confirm({
       title: "確認",
       icon: <ExclamationCircleOutlined />,
@@ -102,68 +108,127 @@ class RoomEdit extends React.Component<Props, Mystate> {
       onOk() {
         return new Promise((resolve, reject) => {
           setTimeout(Math.random() > 0.5 ? resolve : reject, 1000)
-        }).catch(() => console.log("Oops errors!"))
+        })
+          .then(() => {
+            let name: string = ""
+            if (type !== "New")
+              setRoomList(
+                roomList.map(room => {
+                  if (room === selectedRoom) {
+                    room.capacity = changeData.capacity
+                    room.name = changeData.name
+                    name = room.name
+                  }
+                  return room
+                })
+              )
+            else {
+              setRoomList(roomList.concat(changeData))
+            }
+            setIsVisible(false)
+          })
+          .catch(() => {
+            showErrorMessage("變更失敗!")
+          })
       },
       onCancel() {},
     })
   }
 
-  render() {
-    return (
-      <div className="smallbox">
-        <Form
-          {...layout}
-          ref={this.formRef}
-          name="control-ref"
-          onFinish={this.onFinish}
-        >
-          <Row justify="center">
-            <h1>From</h1>
-          </Row>
-          <Form.Item
-            name="select"
-            label="Select room"
-            rules={[{ required: true }]}
-          >
-            <Select
-              showSearch
-              placeholder="Select a room"
-              onChange={this.onSelectRoom}
-              allowClear
-            >
-              {this.state.roomList.map(item => (
-                <Option value={item.name} key={item.name}>
-                  {item.name}
-                </Option>
-              ))}
-            </Select>
-          </Form.Item>
-          <Form.Item label="roomName">
-            <span className="ant-form-text">
-              {this.state.selectedRoom.name}
-            </span>
-          </Form.Item>
-          <Form.Item label="Capacity">
-            <span className="ant-form-text">
-              {this.state.selectedRoom.capacity <= 0
-                ? ""
-                : this.state.selectedRoom.capacity}
-            </span>
-          </Form.Item>
-          <Form.Item {...tailLayout}>
-            <Space size="middle">
-              <Button onClick={this.onCancel} type="default">
-                Cancel
-              </Button>
-              <Button onClick={this.onSave} type="primary" htmlType="submit">
-                Save
-              </Button>
-            </Space>
-          </Form.Item>
-        </Form>
-      </div>
-    )
+  const onFinish = (values: any) => {
+    if (changeData.name === "") showErrorMessage("尚未輸入房間名稱!")
+    else if (changeData.capacity === 0) showErrorMessage("尚未輸入房間可容量人數!")
+    else if (changeData.name !== selectedRoom.name || changeData.capacity !== selectedRoom.capacity)
+      showPromiseConfirm()
+    else showErrorMessage("尚未變更!")
+    console.log(values)
   }
+
+  const onFinishFailed = () => {
+    showErrorMessage("請選擇房間!")
+    console.log("error")
+  }
+
+  const onCancel = () => {
+    setIsVisible(false)
+    form.resetFields()
+  }
+
+  const onSelectRoom = (value: string) => {
+    roomList.forEach(item => {
+      if (item.name === value) {
+        setSelectRoom(item)
+      }
+    })
+  }
+
+  function changeRoomName(e: React.ChangeEvent<HTMLInputElement>) {
+    changeData.name = e.target.value
+    console.log(changeData)
+  }
+
+  function changeRoomCapacity(value: number | string | undefined) {
+    if (value !== undefined) changeData.capacity = Number(value)
+    console.log(changeData)
+  }
+
+  function limitDecimals(value: number | string | undefined) {
+    if (value !== undefined) return String(value).replace(/^(0+)|[^\d]+/g, "")
+    return ""
+  }
+
+  return (
+    <Modal visible={isVisible} okText={type} onOk={onFinish} onCancel={onCancel} footer={false}>
+      <Form {...layout} form={form} name="control-hooks" onFinish={onFinish} onFinishFailed={onFinishFailed}>
+        <Row justify="center">
+          <h1>{type === "New" ? "新增房間" : "變更房間"}</h1>
+        </Row>
+        <Form.Item
+          name="select"
+          label="Select room"
+          rules={[{ required: type !== "New", message: "Select room is require" }]}
+          hasFeedback
+          hidden={type === "New"}
+        >
+          <Select showSearch placeholder="Select a room" onChange={onSelectRoom} allowClear>
+            {roomList.map(item => (
+              <Option value={item.name} key={item.name}>
+                {item.name}
+              </Option>
+            ))}
+          </Select>
+        </Form.Item>
+        <Form.Item label="roomName" name="roomName">
+          <Input readOnly={type === "delete"} onChange={changeRoomName} type="text" />
+        </Form.Item>
+        <Form.Item label="Capacity" name="Capacity">
+          <InputNumber
+            readOnly={type === "delete"}
+            onChange={changeRoomCapacity}
+            type="text"
+            formatter={limitDecimals}
+            parser={limitDecimals}
+            min={0}
+            max={1000}
+          />
+        </Form.Item>
+        <Form.Item {...tailLayout}>
+          <Space size="middle">
+            <Button onClick={onCancel} type="default">
+              Cancel
+            </Button>
+            <Button type="primary" htmlType="submit" danger={type === "delete"}>
+              {type}
+            </Button>
+          </Space>
+        </Form.Item>
+      </Form>
+    </Modal>
+  )
+}
+
+RoomEdit.defaultProps = {
+  visible: true,
 }
 
 export default RoomEdit
--- src/pages/ManagerEdit.tsx
+++ src/pages/ManagerEdit.tsx
@@ -1,12 +1,74 @@
-import React from "react"
-import Edit from "../component/ManagerEdit/Edit"
+import { Button } from "antd"
+import React, { useState } from "react"
 import RoomEdit from "../component/ManagerEdit/roomEdit"
+import { Room } from "../component/utils/interface"
 
+const fakedata = [
+  {
+    name: "TR200",
+    capacity: 12,
+  },
+  {
+    name: "TR500",
+    capacity: 10,
+  },
+  {
+    name: "RB500",
+    capacity: 20,
+  },
+]
 export default function Home() {
+  const [roomList, setroomList] = useState<Array<Room>>(fakedata)
+  const [editSaveFormVisible, setSaveEditFormVsible] = useState(false)
+  const [editNewFormVisible, setNewEditFormVsible] = useState(false)
+  const [editDeleteFormVisible, setDeleteEditFormVsible] = useState(false)
+
   return (
-    <div>
-      <Edit />
-      <RoomEdit name="test" />
-    </div>
+    <>
+      <div>
+        <Button
+          onClick={() => {
+            setSaveEditFormVsible(true)
+          }}
+        >
+          Save
+        </Button>
+        <Button
+          onClick={() => {
+            setNewEditFormVsible(true)
+          }}
+        >
+          New
+        </Button>
+        <Button
+          onClick={() => {
+            setDeleteEditFormVsible(true)
+          }}
+        >
+          Delete
+        </Button>
+      </div>
+      <RoomEdit
+        type="Save"
+        roomList={roomList}
+        setRoomList={setroomList}
+        setvisible={setSaveEditFormVsible}
+        visible={editSaveFormVisible}
+      />
+      <RoomEdit
+        type="New"
+        roomList={roomList}
+        setRoomList={setroomList}
+        setvisible={setNewEditFormVsible}
+        visible={editNewFormVisible}
+      />
+      <RoomEdit
+        type="Delete"
+        roomList={roomList}
+        setRoomList={setroomList}
+        setvisible={setDeleteEditFormVsible}
+        visible={editDeleteFormVisible}
+      />
+    </>
   )
 }
