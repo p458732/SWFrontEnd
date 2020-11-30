d240b45cf195c0919026bd0367913cec7c7b58f4
Zong Wei Hong <p458732@kimo.com>
2020-11-30 16:59:02 +0800

    refactor

---
+++ src/component/scheduler/fetchRoom.tsx
@@ -0,0 +1,34 @@
+import GSTC from "gantt-schedule-timeline-calendar"
+import { Room } from "../utils/interface"
+
+const roomURL = "https://hw.seabao.ml/api/room"
+
+function generateNewRows(room: Array<Room>) {
+  const rows: any = {}
+  for (let i = 0; i < room.length; i += 1) {
+    const id = GSTC.api.GSTCID(room[i].name)
+    rows[id] = {
+      id,
+      label: room[i].name,
+      capacity: room[i].capacity,
+      expanded: false,
+    }
+  }
+  console.log(rows)
+  return rows
+}
+function getRoom(state: any) {
+  fetch(roomURL, {
+    method: "GET",
+  })
+    .then(response => {
+      return response.json()
+    })
+    .then((roomData: Array<Room>) => {
+      state.update("config.list.rows", () => {
+        return generateNewRows(roomData)
+      })
+    })
+}
+
+export default getRoom
